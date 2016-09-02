9b542692c936bd339a2f5cf0fdc395d4b767d337
Dimitris <dimitrisbor@gmail.com>
2016-09-03 01:41:11 +0300

    movies initial code

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,19 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $location) {
+.controller('AppCtrl', function($scope, $location, $http) {
     $scope.isItemActive = function(item) {
         return $location.path().indexOf(item.href) > -1;
     };
+
+    $scope.images = [];
+
+    $scope.loadImages = function() {
+        $http.get('js/list_movies.json')
+            .then(function(res){
+                $scope.movies = res.data.data.movies;
+            });
+
+    }
 
 
 })
